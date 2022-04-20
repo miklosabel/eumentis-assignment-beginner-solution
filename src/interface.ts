dde79191ad5c3b9b58689ae53255740535e88566
Miklós Ábel <miklos.abel@encosoft.hu>
2022-04-20 12:50:00 +0200

    created user type

---
+++ src/interface.ts
@@ -0,0 +1,18 @@
+export interface User {
+	id: string;
+	username: string;
+	name: string;
+	email: string;
+	phone: string;
+	address: Address;
+	company: {
+		name: string;
+	}
+}
+
+interface Address {
+	street: string;  // Address line 1 
+	suite: string;  // Address line 2 
+	city: string;
+	zipcode: string;
+}
