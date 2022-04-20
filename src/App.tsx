f919d0a1462cc925e9f1a91c24395c23ee96b967
Miklós Ábel <miklos.abel@encosoft.hu>
2022-04-20 12:50:18 +0200

    data fetching and rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { User } from './interface';
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://jsonplaceholder.typicode.com/users');
+      setUsers(result.data);
+    }
+    fetchData();
+  }, [])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map(user => <div>{JSON.stringify(user)}</div>)}
     </div>
   );
 }
